8b9a884bdd421a4cbd644aebaaa47cc9ff5a8f21
Leandro <leandrouni01@gmail.com>
2021-09-14 12:29:00 -0300

    Create SQLI-sub-capsula-3-ej.sql

---
+++ SQLI-sub-capsula-3-ej.sql
@@ -0,0 +1,12 @@
+--1
+SELECT nombre, apellido, fecha_nacimiento FROM PROFESOR ORDER BY fecha_nacimiento ;
+--2
+SELECT * FROM PROFESOR WHERE salario >= 65000;
+--3
+SELECT * FROM PROFESOR WHERE fecha_nacimiento BETWEEN '1980/1/01' AND '1989/12/31';
+--4
+SELECT * FROM PROFESOR LIMIT 5;
+--5
+SELECT * FROM PROFESOR WHERE apellido LIKE 'P%';
+--6
+SELECT * FROM PROFESOR WHERE fecha_nacimiento BETWEEN '1980/1/01' AND '1989/12/31' AND salario > 80000;
